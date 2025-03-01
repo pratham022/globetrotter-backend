f403af7338e4d475f1fa6f2d4a944789fd2bbeff
Prathamesh <pthombre657@gmail.com>
2025-03-02 01:58:54 +0530

    add cors

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "https://globetrotter-frontend.onrender.com" }));
 app.use(express.json());
 
 // Routes
